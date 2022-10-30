1c177e7bac1f87d2b16bd0323ac9b14e0d0e553c
Wongbuer <wongbuer@outlook.com>
2022-10-30 12:37:39 +0800

    C/S两层架构改造完成

--- src/main/java/com/wong/contact_backend/common/CommonResponse.java
+++ src/main/java/com/wong/contact_backend/common/CommonResponse.java
@@ -8,4 +8,20 @@ public class CommonResponse<T> {
         this.status = status;
         this.data = data;
     }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
